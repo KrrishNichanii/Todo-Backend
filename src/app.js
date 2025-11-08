be02ccf672f803baf75e9a098687273b0657c4bb
KrrishNichanii <krrishnichanii@gmail.com>
2025-11-08 16:49:59 +0530

    updated auth service

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(cookieParser()) ;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5174', 
-  'https://todo-frontend-5bhv357ua-krrish-nichaniis-projects.vercel.app/', // production frontend
+  'https://todo-frontend-ebswob8ff-krrish-nichaniis-projects.vercel.app/', // production frontend
 ];
 
 const corsOptions = {
